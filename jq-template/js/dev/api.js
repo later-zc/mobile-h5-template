4193cacbf89f3d3b46ec307619780e562d9d0fb8
2363442097@qq.com <2363442097@qq.com>
2023-03-19 01:36:36 +0800

    bulid: 新增jquery-mockjax插件模拟请求响应、easy less扩展在保存时压缩配置

---
+++ jq-template/js/dev/api.js
@@ -0,0 +1,33 @@
+$.mockjax({
+  url: 'Common/init.aspx',
+  response: function (settings) {
+    this.responseText = {
+      status: 1,
+      fund: 1, //用户可抽奖次数
+      first: 0, //用户分享情况{已分享:1；未分享：0；默认未分享}
+    }
+  },
+})
+$.mockjax({
+  url: 'Common/form2.aspx',
+  response: function (settings) {
+    this.responseText = {
+      status: 1,
+    }
+  },
+})
+$.mockjax({
+  url: 'Common/form3.aspx',
+  response: function (settings) {
+    this.responseText = {
+      status: 1,
+    }
+  },
+})
+$.mockjax({
+  url: 'Common/form.aspx',
+  responseText: {
+    status: 1,
+    list: [{ name: '百货1000减60优惠券' }],
+  },
+})
